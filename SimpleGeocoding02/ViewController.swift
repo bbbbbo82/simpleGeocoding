5ccd173013bfefa716dcfd53df0afce27fad58eb
D7703_15 <d7703_15@d7703-15ui-imac.local>
2019-10-14 14:25:51 +0900

    Initial Commit

---
+++ SimpleGeocoding02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleGeocoding02
+//
+//  Created by D7703_15 on 2019. 10. 14..
+//  Copyright © 2019년 BlueScreen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
